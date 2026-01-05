0adcc5e59e13cedb7f5e8f88c777358cf52eca9a
Tufan Akbaş <tufanakbastr@gmail.com>
2026-01-05 22:50:57 +0300

    Initial Commit

---
+++ SmartHomeControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartHomeControl
+//
+//  Created by Tufan Akbaş on 5.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
